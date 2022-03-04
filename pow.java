691621d995090b3380d13dc65f29492379dbd603
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-03-04 23:57:09 +0530

    math pow in java

---
+++ pow.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class pow {
+public static void main(String[] args){
+    System.out.println("enter a number:");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.println("power of no="+Math.pow(a,7));
+}
+}
