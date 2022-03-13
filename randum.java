e851b397cccf1f1f8908837c8c923def88e9e0fe
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-03-13 21:49:46 +0530

    to get randum number using randum fun

---
+++ randum.java
@@ -0,0 +1,7 @@
+//to print random number in java.
+public class randum {
+    public static void main(String[] args){
+        int ramdum=(int)(Math.random()*3);  //15 is exclusive.
+        System.out.print(ramdum);
+    }
+}
