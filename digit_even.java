2ad7cce13cd39891750b17b56cf3533f39c3a608
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-03-13 21:11:46 +0530

    even digit leet code

---
+++ digit_even.java
@@ -0,0 +1,15 @@
+public class digit_even {
+    public static void main(String[] args) {
+        int[] arry = {1, 4, 5, 6, 0 };
+        int [] arr2=new int[5];
+        int i,j,k=0;
+        for(i=0;i<3;i++)
+        {   if(arry[i]==0)
+            continue;
+            for(j=0;j<5;j++)
+            {
+                if(arry[i]!=0 && arry[i+2]%2==0)// not solved.
+                arr2[k++] =
+            }
+        }
+}
