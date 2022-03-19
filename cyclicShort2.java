a6172ff543d94e8e2af7d2b030e8b7403fd8ea9d
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-03-19 22:55:51 +0530

    cyclic short of array using another method.

---
+++ cyclicShort2.java
@@ -0,0 +1,25 @@
+// Cyclic shorting of arry note this is will work if array is starting from 1 
+import java.util.*;
+public class cyclicShort2 {
+    public static void main(String[] args){
+        int [] arry={2,7,9,3,4,6,1,5};
+        shorting(arry);   // for shorting arry in assending order.
+        System.out.println(Arrays.toString(arry));
+    }
+    public static void shorting(int [] arry){
+        int i=0,index;
+        while(i<arry.length)
+        {
+            index=arry[i]-1;
+            if(arry[i]<arry.length&&arry[i]!=arry[index])  // we can swap here.
+            swap(arry,i,index);
+            else
+            i++;
+        }
+    }
+    public static void swap(int[] arry, int i, int index){
+        int temp= arry[i];     //if index not at correct place then we swap it.
+        arry[i]=arry[index];
+        arry[index]=temp;
+    }
+}
