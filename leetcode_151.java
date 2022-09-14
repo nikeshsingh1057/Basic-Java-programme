2f0bdc6b4f86f0776045f37ff9ca1e6dffa731a8
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-09-14 22:49:41 +0530

    Create leetcode_151.java

---
+++ leetcode_151.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+public class reverse_word
+{
+public static void main(String[] args) {
+	
+	String s="world aasdfasdf hu apple";
+    String s2="";
+    for(int i=s.length()-1;i>=0;i--)
+    {
+        if(s.charAt(i)==' ')
+        continue;
+        
+        else if(s.charAt(i)!=' ')
+        {
+        	String s3="";
+            s3=reverse(i,s,s3);
+            s2+=s3;
+            i=i-s3.length();
+        }
+         s2+=" ";
+    }
+    System.out.println(s2.trim());
+}
+public static String reverse(int i, String s,String s3)
+{
+    Stack<Character> st=new Stack<>();
+    while(i>=0 && s.charAt(i)!=' ')
+    {
+        st.push(s.charAt(i));
+        i--;
+    }
+    while(st.isEmpty()==false)
+     s3+=st.pop();
+    return s3;
+    
+}
+}
+
