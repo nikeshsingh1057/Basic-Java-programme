9dfad27c052de7944b96b922081da1a0f7468629
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-12-01 11:04:33 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        if(n==1)
+        return;
+        long arr1[]=new long[n];
+        
+        int i=0,j=arr.length-1;
+        
+        int k=0;
+        while(i<j)
+        {
+            arr1[k]=arr[j];
+            arr1[k+1]=arr[i];
+            
+            k+=2;
+            
+            i++;
+            j--;
+        }
+        
+        if(n%2!=0)
+            arr1[k]=arr[i];
+            
+        for(int l=0;l<n;l++)
+        {
+            arr[l]=arr1[l];
+        }
+    }
+    
+}
+
+
