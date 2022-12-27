5f806b4477006b315c1d6b256c6fed7e8c00cfed
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-12-27 20:03:05 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (84.01%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -8,11 +8,11 @@ class Solution {
         {
             int mid=low+(high-low)/2;
             
-            if(mid>0 && arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1])
-                return mid;
+            if(mid>0 && arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]) // if we remove it than it also work.
+                return mid;         
             
            else if(arr[mid]>arr[mid+1])
-                high=mid;
+                high=mid-1;
             
            else if(arr[mid]<arr[mid+1])
                 low=mid+1;
