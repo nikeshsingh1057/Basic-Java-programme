358224d68c780f40a30e871a086a361aa859c220
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-02-01 21:53:32 +0530

    Time: 21 ms (72.28%), Space: 43.4 MB (85.20%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,28 +1,34 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ll=new ArrayList<>();
-        combination(n,k,0,1,ans,ll);
+    List<List<Integer>> ans=new ArrayList<>();
+    List<Integer> ll=new ArrayList<>();
         
+        int arr[]=new int[n];
+		comb(arr,k,0,0,ans,ll);
         return ans;
     }
-    public static void combination(int n,int k,int count,int j,List<List<Integer>> ans,List<Integer> ll)
-	{
-        // if(count>k)     // we can remove it also
-        //     return;
+    
+    public static void comb(int arr[],int k,int count,int j,List<List<Integer>> ans, List<Integer> ll)
         
+	{
 		if(count==k)
 		{
 			ans.add(new ArrayList<>(ll));
 			return;
 		}
-		
-		for(int i=j;i<=n;i++)
+		for(int i=j;i<arr.length;i++)
 		{
-            ll.add(i);
-			combination(n,k,count+1,i+1,ans,ll);
-            ll.remove(ll.size()-1);
+			if(arr[i]==0) {
+				
+				arr[i]=1;
+                ll.add(i+1);
+                
+				comb(arr,k,count+1,i+1,ans,ll);
+                
+				arr[i]=0;
+                ll.remove(ll.size()-1);
+			}
 		}
 	}
 }
