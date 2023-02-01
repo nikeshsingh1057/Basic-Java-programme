0dac47f40bb2a57761a00e1e191866c121609d87
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-02-01 15:01:56 +0530

    Time: 20 ms (74.28%), Space: 43.4 MB (85.20%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ll=new ArrayList<>();
+        combination(n,k,0,1,ans,ll);
+        
+        return ans;
+    }
+    public static void combination(int n,int k,int count,int j,List<List<Integer>> ans,List<Integer> ll)
+	{
+        if(count>k)
+            return;
+		if(count==k)
+		{
+			ans.add(new ArrayList<>(ll));
+			return;
+		}
+		
+		for(int i=j;i<=n;i++)
+		{
+            ll.add(i);
+			combination(n,k,count+1,i+1,ans,ll);
+            ll.remove(ll.size()-1);
+		}
+	}
+}
